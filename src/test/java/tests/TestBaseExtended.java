c2957884eabc4c6822ad252278b02da9359619b6
Andrew <mr.kimpa4i@gmail.com>
2025-06-03 15:45:35 +0300

    added attach

--- src/test/java/tests/TestBaseExtended.java
+++ src/test/java/tests/TestBaseExtended.java
@@ -2,17 +2,15 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import demoqa.helpers.Attach;
+import pages.RegistrationPage;
+
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
+
 import java.util.Map;
 
 
@@ -38,10 +36,18 @@ public class TestBaseExtended {
     }
 
     @BeforeEach
-    void addListener(){
-        SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
 
 
 }
